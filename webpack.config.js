4dd9996eb375ec0f6f49d6621ab922798defdfd5
Pascal Barth <ltbap@MacBook-Pro-Swisstopo-Pascal-Barth.local>
2019-01-10 14:37:08 +0100

    Project boostraping
    Adding a fonctionnal OL map with a react Component. Need more debugging on checkboxes

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.less$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            //modifyVars: CustomAntThemeModifyVars(),
+                            javascriptEnabled: true // Less version > 3.0.0
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: 'url-loader?limit=10000',
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    'file-loader?name=images/[name].[ext]',
+                    'image-webpack-loader?bypassOnDebug'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
